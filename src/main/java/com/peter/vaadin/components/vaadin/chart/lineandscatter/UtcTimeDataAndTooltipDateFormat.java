3e08c57a9eb85f7996ecbdb9f2ab49009541a08d
PeterDG <peterdavid_gutierrez@hotmail.com>
2015-12-17 13:30:23 -0500

    add new methods for update chart UTCTimeData

--- src/main/java/com/peter/vaadin/components/vaadin/chart/lineandscatter/UtcTimeDataAndTooltipDateFormat.java
+++ src/main/java/com/peter/vaadin/components/vaadin/chart/lineandscatter/UtcTimeDataAndTooltipDateFormat.java
@@ -5,14 +5,25 @@ import com.vaadin.addon.charts.model.*;
 import com.peter.vaadin.components.vaadin.chart.AbstractVaadinChartExample;
 import com.vaadin.ui.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UtcTimeDataAndTooltipDateFormat extends AbstractVaadinChartExample {
 
     public List<DataSeries> dataSeriesList;
-
+    public Configuration configuration;
+    public Chart chart;
     public UtcTimeDataAndTooltipDateFormat(List<DataSeries> dataSeriesList) {
         this.dataSeriesList = dataSeriesList;
+        chart = new Chart();
+        chart.setHeight("450px");
+        chart.setWidth("100%");
+        configuration = chart.getConfiguration();
+        configuration.setTitle("Time and date in tooltip");
+        configuration.getChart().setType(ChartType.SPLINE);
+        configuration.getxAxis().setType(AxisType.DATETIME);
+        // Finnish convention for date formatting
+        configuration.getTooltip().setxDateFormat("%d.%m. %Y %H:%M");
     }
 
     @Override
@@ -22,24 +33,28 @@ public class UtcTimeDataAndTooltipDateFormat extends AbstractVaadinChartExample 
 
     @Override
     protected Component getChart() {
-        Chart chart = new Chart();
-        chart.setHeight("450px");
-        chart.setWidth("100%");
+        setDataSeriesList(dataSeriesList);
+        chart.drawChart(configuration);
+        return chart;
+    }
 
-        Configuration configuration = chart.getConfiguration();
-        configuration.setTitle("Time and date in tooltip");
-        configuration.getChart().setType(ChartType.SPLINE);
+    public void setTitle(String title){
+        configuration.setTitle(title);
+        chart.drawChart(configuration);
+    }
 
-        configuration.getxAxis().setType(AxisType.DATETIME);
-
-        // Finnish convention for date formatting
-        configuration.getTooltip().setxDateFormat("%d.%m. %Y %H:%M");
+    public void setDataSeriesList(List<DataSeries> dataSeriesList){
         for(DataSeries series: dataSeriesList){
             configuration.addSeries(series);
         }
+    }
 
+    public void update(List<DataSeries> dataSeriesList){
+        configuration.setSeries(new ArrayList<Series>());
+        setDataSeriesList(dataSeriesList);
         chart.drawChart(configuration);
-        return chart;
+//        getUI().access(() ->
+//                this.dataSeriesList=dataSeriesList);
     }
 
 }
